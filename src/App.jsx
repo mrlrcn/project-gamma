a16af16ba45e32e3e7359db11bd51c0eaac325ba
Rosa <=>
2023-04-20 21:00:26 +0200

    Para Celia

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './scss/style.scss'
+import Topbar from './components/topbar/Topbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Topbar />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,12 @@
+import './Topbar.scss'
+
+function Topbar() {
+  
+    return (
+
+      <h1 className="topbar">HOLA</h1>
+      
+    )
+  }
+
+  export default Topbar
